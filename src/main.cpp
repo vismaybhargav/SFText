79d1b13b8bce32f567ff0bef0291d775608a0fcc
Vismay Bhargav <vismay.bhargav@gmail.com>
2025-05-03 02:46:03 -0700

    (feat) backspacing, using arrow keys, better formatting, lots of things

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,21 @@ int main() {
     }
 
     const sf::Glyph exampleGlyph { font.getGlyph('A', 30, false) };
-    const float line_height{exampleGlyph.bounds.size.y + 5.f};
+    const float line_height{exampleGlyph.bounds.size.y + 10.f};
 
     std::vector<sf::String> text_lines;
     text_lines.emplace_back("");
 
     sft::Cursor cursor {0 , 0};
     sf::RectangleShape cursor_rect {sf::Vector2f{5, exampleGlyph.bounds.size.y + 10.f}};
+    char32_t char_typed;
 
     sf::Clock delta_clock;
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
+            if (event.has_value()) {
+                ImGui::SFML::ProcessEvent(window, event.value());
+            }
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             } else if (const auto* key_pressed = event->getIf<sf::Event::KeyPressed>()) {
@@ -38,7 +42,7 @@ int main() {
                         if (cursor.row > 0) {
                             cursor.row--;
                             text_lines.pop_back();
-                            cursor.col = static_cast<int>(text_lines.back().getSize()) - 1;
+                            cursor.col = static_cast<int>(text_lines.back().getSize());
                         }
                     } else {
                         sf::String str = text_lines.at(cursor.row);
@@ -50,31 +54,45 @@ int main() {
                     cursor.row++;
                     cursor.col = 0;
                 } else if (key_pressed->scancode == sf::Keyboard::Scancode::Left) {
-
+                    if (cursor.col > 0) {
+                        cursor.col--;
+                    }
                 } else if (key_pressed->scancode == sf::Keyboard::Scancode::Right) {
-
+                    if (cursor.col < text_lines.at(cursor.row).getSize()) {
+                        cursor.col++;
+                    }
                 } else if (key_pressed->scancode == sf::Keyboard::Scancode::Up) {
-
+                    if (cursor.row > 0) {
+                        cursor.row--;
+                    }
                 } else if (key_pressed->scancode == sf::Keyboard::Scancode::Down) {
-
+                    if (cursor.row < text_lines.size()) {
+                        cursor.row++;
+                    }
                 }
             } else if (const auto* text_entered = event->getIf<sf::Event::TextEntered>()) {
-                if (text_entered->unicode != 8) {
-                    text_lines.at(cursor.row) += text_entered->unicode;
+                char_typed = text_entered->unicode;
+                // 8 -> Backspace, 13 -> Enter key
+                if (char_typed != 8 && char_typed != 13) {
+                    if (cursor.col == text_lines.at(cursor.row).getSize()) {
+                        text_lines.at(cursor.row) += char_typed;
+                    } else {
+                        text_lines.at(cursor.row).insert(cursor.col, char_typed);
+                    }
                     cursor.col++;
-                    cursor_rect.setPosition(
-                        {
-                            cursor_rect.getPosition().x + exampleGlyph.bounds.size.x + exampleGlyph.advance,
-                            cursor_rect.getPosition().y\
-                        });
                 }
             }
         }
 
+        cursor_rect.setPosition({cursor.col * exampleGlyph.advance, cursor.row * line_height});
+
         ImGui::SFML::Update(window, delta_clock.restart());
 
+        ImGui::Begin("Test");
+        ImGui::Text("UNICODE: " + char_typed);
         ImGui::Text(("ROW: " + std::to_string(cursor.row)).c_str());
         ImGui::Text(("COLUMN: " + std::to_string(cursor.col)).c_str());
+        ImGui::End();
 
         window.clear();
         for (int i = 0; i < text_lines.size(); i++) {
@@ -84,6 +102,7 @@ int main() {
         }
         window.draw(cursor_rect);
         ImGui::SFML::Render(window);
+        ImGui::EndFrame();
         window.display();
     }
 
